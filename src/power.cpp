1a1a7ed8db6dea5fcccc8a03ee3eacfb67dbc9d7
lateishashahril <lateishashahril@vt.edu>
2026-02-08 16:59:11 -0500

    fsm states

---
+++ src/power.cpp
@@ -0,0 +1,10 @@
+#include "indicators.h"
+
+void initIndicators() {
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void blinkYellow() { /* timing logic */ }
+void blinkRed() { /* timing logic */ }
+void soundBuzzer() { /* GPIO on/off */ }
+
