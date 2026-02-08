16e772b9363b1be1542606ffc86f430db38bc051
lateishashahril <lateishashahril@vt.edu>
2026-02-08 16:59:26 -0500

    fsm

--- src/fsm.cpp
+++ src/fsm.cpp
@@ -12,3 +12,79 @@ enum SystemState {
     STATE_BP_CRITICAL,
     STATE_DONE
 };
+
+static SystemState currentState = STATE_OFF;
+
+void initFSM() {
+    currentState = STATE_INIT;
+}
+
+void runFSM() {
+    switch (currentState) {
+
+    case STATE_OFF:
+        if (powerSwitchOn())
+            currentState = STATE_INIT;
+        break;
+
+    case STATE_INIT:
+        initSensors();
+        initIndicators();
+        initRTC();
+
+        if (isBatteryLow())
+            currentState = STATE_LOW_BATT;
+        else
+            currentState = STATE_IDLE;
+        break;
+
+    case STATE_LOW_BATT:
+        blinkYellow();
+        if (isCharging())
+            currentState = STATE_CHARGING;
+        break;
+
+    case STATE_CHARGING:
+        blinkYellow();
+        if (!isCharging() && getBatteryPercent() > 30)
+            currentState = STATE_INIT;
+        break;
+
+    case STATE_IDLE:
+        enterLowPowerMode();
+        if (rtcTriggered())
+            currentState = STATE_CONDITION_CHECK;
+        break;
+
+    case STATE_CONDITION_CHECK:
+        if (isUserStill() && isPositionValid())
+            currentState = STATE_MEASURE_BP;
+        else {
+            scheduleRetry(20);
+            currentState = STATE_IDLE;
+        }
+        break;
+
+    case STATE_MEASURE_BP:
+        BPStatus status = measureBP();
+
+        if (status == BP_CRITICAL)
+            currentState = STATE_BP_CRITICAL;
+        else if (status == BP_HIGH)
+            currentState = STATE_BP_HIGH;
+        else
+            currentState = STATE_DONE;
+        break;
+
+    case STATE_BP_HIGH:
+        blinkRed();
+        currentState = STATE_DONE;
+        break;
+
+    case STATE_BP_CRITICAL:
+        blinkRed();
+        soundBuzzer();
+        currentState = STATE_DONE;
+        break;
+    }
+}
---
+++ src/indicators.cpp
@@ -0,0 +1,9 @@
+#include "indicators.h"
+
+void initIndicators() {
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void blinkYellow() { /* timing logic */ }
+void blinkRed() { /* timing logic */ }
+void soundBuzzer() { /* GPIO on/off */ }
