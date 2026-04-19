f60b5fe78cb32e5c983ca3a54e228873e7e58095
allisonh26 <142938007+allisonh26@users.noreply.github.com>
2026-04-19 11:00:06 -0400

    added indicator functions

--- lib/indicators/indicators.cpp
+++ lib/indicators/indicators.cpp
@@ -1,28 +1,74 @@
 #include "indicators.h"
 
-#define LED_RED 2
-#define LED_YELLOW 3
-#define BUZZER_PIN 4
+#define LED_RED 1
+#define LED_BLUE 2
+#define LED_GREEN 3
+#define BUZZER_PIN 0
+#define BUTTON_PIN 7
 
 void Indicators::init() {
     pinMode(LED_RED, OUTPUT);
-    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
     pinMode(BUZZER_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
 }
 
 void Indicators::blinkYellow(int durationMs) {
     unsigned long start = millis();
+    digitalWrite(LED_BLUE, HIGH);
     while (millis() - start < durationMs) {
-        digitalWrite(LED_YELLOW, HIGH);
+        digitalWrite(LED_GREEN, LOW);
+        digitalWrite(LED_RED, LOW);
+        delay(250);
+
+        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(LED_RED, HIGH);
+        delay(250);
+    }
+}
+
+void Indicators::testRGB(int durationMs) {
+    unsigned long start = millis();
+    while (millis() - start < durationMs) {
+        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(LED_RED, HIGH);
+        digitalWrite(LED_BLUE, LOW);
         delay(500);
-        digitalWrite(LED_YELLOW, LOW);
+
+        digitalWrite(LED_GREEN, LOW);
+        digitalWrite(LED_RED, HIGH);
+        digitalWrite(LED_BLUE, HIGH);
+        delay(500);
+
+        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(LED_RED, LOW);
+        digitalWrite(LED_BLUE, HIGH);
         delay(500);
     }
 }
 
-void Indicators::alertHighBP() { digitalWrite(LED_RED, HIGH); }
+void Indicators::alertHighBP() { digitalWrite(LED_BLUE, LOW); }
+
+void Indicators::ledOff() {
+    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+}
 
 void Indicators::alertCriticalBP() {
-    digitalWrite(LED_RED, HIGH);
-    digitalWrite(BUZZER_PIN, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+
+    digitalWrite(LED_RED, LOW);
+    tone(BUZZER_PIN, 4000, 500);
+    delay(500);
+    // digitalWrite(LED_RED, HIGH);
+    // tone(BUZZER_PIN, 4000, 500);
+    // delay(500);
+}
+
+bool Indicators::detectButton() {
+    bool buttonState = digitalRead(BUTTON_PIN);
+    return buttonState;
 }
