7fc0a50eb462e4bb39a9a525180d98fd6b991ff2
samuraisamuel <samuelt22@vt.edu>
2026-05-04 01:51:06 -0400

    added ppg wrapper class, ready to be implimented.

---
+++ lib/ppg/ppg.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file ppg.cpp
+ * @author Samuel Thompson,
+ * @description: PPG class implementation.
+ * @date 2026/05/04
+ * @modified:
+ */
+
+#include "ppg.hpp"
+
+bloodPressure PPG::run() {
+    /** @todo Jisu implement your PPG code in a single run function here; that
+     * turn's everything on, and everything off, and returns the results.
+     *  */
+
+    bloodPressure results;
+
+    results.systolic = 100;  // temp
+    results.diastolic = 100; // temp
+
+    return results;
+}
